d19ed0987c76e6850880b35b8ca4ec9518510ea7
Harshada Naik <68236767+naikharshada@users.noreply.github.com>
2023-05-04 14:05:32 +0530

    Add files via upload

---
+++ ArithmeticCalculator/ArithmeticCalculator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace ArithmeticCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblHeading = new System.Windows.Forms.Label();
+            this.pnlInput = new System.Windows.Forms.Panel();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.LblNum2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.LblNum1 = new System.Windows.Forms.Label();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pnlInput.SuspendLayout();
+            this.pnlButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LblHeading
+            // 
+            this.LblHeading.BackColor = System.Drawing.Color.White;
+            this.LblHeading.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LblHeading.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblHeading.Location = new System.Drawing.Point(29, 9);
+            this.LblHeading.Name = "LblHeading";
+            this.LblHeading.Size = new System.Drawing.Size(400, 51);
+            this.LblHeading.TabIndex = 0;
+            this.LblHeading.Text = "Arithmetic Calculator";
+            this.LblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlInput
+            // 
+            this.pnlInput.Controls.Add(this.txtNum2);
+            this.pnlInput.Controls.Add(this.LblNum2);
+            this.pnlInput.Controls.Add(this.txtNum1);
+            this.pnlInput.Controls.Add(this.LblNum1);
+            this.pnlInput.Location = new System.Drawing.Point(29, 82);
+            this.pnlInput.Name = "pnlInput";
+            this.pnlInput.Size = new System.Drawing.Size(400, 119);
+            this.pnlInput.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(142, 64);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(226, 29);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // LblNum2
+            // 
+            this.LblNum2.AutoSize = true;
+            this.LblNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNum2.Location = new System.Drawing.Point(37, 67);
+            this.LblNum2.Name = "LblNum2";
+            this.LblNum2.Size = new System.Drawing.Size(99, 24);
+            this.LblNum2.TabIndex = 2;
+            this.LblNum2.Text = "Number 2:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(142, 18);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(226, 29);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // LblNum1
+            // 
+            this.LblNum1.AutoSize = true;
+            this.LblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNum1.Location = new System.Drawing.Point(37, 23);
+            this.LblNum1.Name = "LblNum1";
+            this.LblNum1.Size = new System.Drawing.Size(99, 24);
+            this.LblNum1.TabIndex = 0;
+            this.LblNum1.Text = "Number 1:";
+            this.LblNum1.Click += new System.EventHandler(this.LblNum1_Click);
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Controls.Add(this.btnDiv);
+            this.pnlButtons.Controls.Add(this.btnMul);
+            this.pnlButtons.Controls.Add(this.btnSub);
+            this.pnlButtons.Controls.Add(this.btnAdd);
+            this.pnlButtons.Location = new System.Drawing.Point(29, 226);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(400, 80);
+            this.pnlButtons.TabIndex = 2;
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.Location = new System.Drawing.Point(303, 25);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(80, 31);
+            this.btnDiv.TabIndex = 3;
+            this.btnDiv.Text = "Div";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMul.Location = new System.Drawing.Point(208, 25);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(80, 31);
+            this.btnMul.TabIndex = 2;
+            this.btnMul.Text = "Mul";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSub.Location = new System.Drawing.Point(110, 25);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(80, 31);
+            this.btnSub.TabIndex = 1;
+            this.btnSub.Text = "Sub";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(14, 25);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(80, 31);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(171, 353);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(226, 29);
+            this.txtResult.TabIndex = 4;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(66, 358);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(72, 24);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.pnlInput);
+            this.Controls.Add(this.LblHeading);
+            this.Name = "Form1";
+            this.Text = "This is a simple Calculator";
+            this.pnlInput.ResumeLayout(false);
+            this.pnlInput.PerformLayout();
+            this.pnlButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblHeading;
+        private System.Windows.Forms.Panel pnlInput;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label LblNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label LblNum2;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ ArithmeticCalculator/ArithmeticCalculator/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArithmeticCalculator
+{
+    public partial class Form1 : Form
+    {
+        //Declare Instance Variable
+        double varNum1, varNum2, varRes;
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+           /* varNum1 = double.Parse(txtNum1.Text);
+            varNum2 = double.Parse(txtNum2.Text);
+            varRes = varNum1 + varNum2;
+            txtResult.Text = varRes.ToString();*/
+
+            if(!double.TryParse(txtNum1.Text,out varNum1))
+            {
+                MessageBox.Show("Enter a valid Number !");
+                txtNum1.Clear();
+                txtNum1.Focus();
+                return;
+            }
+            if(!double.TryParse(txtNum2.Text,out varNum2))
+            {
+                MessageBox.Show("Enter a valid Number !");
+                txtNum2.Clear();
+                txtNum2.Focus();
+                return;
+            }
+            
+            Button temp = (Button)sender;
+            String operation = temp.Text; //Add, sub, mul, div
+            
+            switch(operation)
+            {
+                case "Add":
+                    varRes = varNum1 + varNum2;
+                    break;
+                case "Sub":
+                    varRes = varNum1 - varNum2;
+                    break;
+                case "Mul":
+                    varRes = varNum1 * varNum2;
+                    break;
+                case "Div":
+                    varRes = varNum1 / varNum2;
+                    break;
+            }
+
+           
+            txtResult.Text = varRes.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LblNum1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
