30c99f17c291179ae9b71001f223b81a00843811
Harshada Naik <68236767+naikharshada@users.noreply.github.com>
2023-05-16 12:50:04 +0530

    Add files via upload

---
+++ Calender/CalenderControl.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalenderControl.aspx.cs" Inherits="Calender.CalenderControl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+        </div>
+              <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Result" OnClick="Button1_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Calender/CalenderControl.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calender
+{
+    public partial class CalenderControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Calendar1.Caption = "Harshada's Calender";
+            Label1.Text = "Todays Date : " + Calendar1.TodaysDate.ToShortDateString();
+            Label2.Text = "Ganpati Vacation Start : 9-13-2023";
+            TimeSpan d = new DateTime(2023, 6, 13) - DateTime.Now;
+            Label3.Text = "Days Remaining for my Birthday : " + d.Days.ToString();
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            Label4.Text = "Your Selected Date : " + Calendar1.SelectedDate.Date.ToString();
+        }
+    }
+}
