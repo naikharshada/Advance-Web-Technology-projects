804fe621116e9d9a94ac22aad1eeabfc9bc829ed
Harshada Naik <68236767+naikharshada@users.noreply.github.com>
2023-05-22 14:13:36 +0530

    Add files via upload

---
+++ FileUploder/Uploader.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Uploader.aspx.cs" Inherits="FileUploder.Uploader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>File Uploader</h2>
+        <div>
+             <asp:FileUpload ID="FileUpload1" runat="server" />
+        </div>
+      
+          <p>  <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" />  </p>
+           <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+            
+    </form>
+</body>
+</html>
+      
+                 
+            
+      
+      
+   
---
+++ FileUploder/Uploader.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileUploder
+{
+    public partial class Uploader : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(FileUpload1.HasFile)
+            {
+                FileUpload1.SaveAs(@"C:\Harshada\AWT labs\" + FileUpload1.FileName);
+                Label1.Text = "File Uploaded Successfully! " + FileUpload1.FileName;
+            }
+            else
+            {
+                Label1.Text = "No File Uploaded!";
+            }
+        }
+    }
+}
